9b1b68ab05ee0cd73086ace7cd69af5f70feb102
Enrique Cornejo <enrique@cornejo.me>
2017-09-25 16:37:13 +0200

    Added a new docker compose file for the processing handling

--- jupyter.sh
+++ jupyter.sh
@@ -5,6 +5,11 @@ case "$1" in
         type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
         docker-compose -f docker-compose.yml build && docker-compose -f docker-compose.yml up
         ;;
+    process)
+        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
+        docker-compose -f docker-compose-process.yml build && docker-compose -f docker-compose-process.yml up
+        ;;
+
   *)
         echo "Usage: jupyter.sh {develop}" >&2
         exit 1

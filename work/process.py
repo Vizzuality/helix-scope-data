deff2bac17d5a2988976f2e895669420ba477e0a
Benjamin A. Laken <benlaken@gmail.com>
2017-09-25 16:56:11 +0200

    adding mutliprocessing script

--- work/process.py
+++ work/process.py
@@ -4,36 +4,23 @@ import time
 import math
 import glob
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from a list l"""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 s = gpd.read_file('./data/gadm28_adm1/gadm28_adm1.shp')
 s = s.to_crs(epsg='4326')
 d = helix_funcs.identify_netcdf_and_csv_files()
 fs = d.get('nc')  # Simply break this list by the number of avilable processors
-                  # and run something like the following for each processor
-
-# for f in fs:
-#   helix_funcs.process_file(file=f, shps=s)
 
 
 if __name__ == "__main__":
     start = time.time()
-    cpus = cpu_count() -1
-    # total_files = files
-    # for nfile in nc_list:
-    #     print('Running on:', nfile[50:])
-    #     processes= [Process(target=MT_Means_Over_Lake, args=(nfile,lake_data,\
-    #                 i,out_path,), kwargs={'plots':False,'rprt_tme':False,})\
-    #                                 for i in range(10)] # Run x lakes
-    #
-    #     cpus = cpu_count() -1
-    #     p_groups= int(math.ceil(float(len(processes))/float(cpus)))
-    #     srng = 0
-    #     frng = cpus
-    #     for n in xrange(p_groups):
-    #         [p.start() for p in processes[srng:frng]]
-    #         [p.join() for p in processes[srng:frng]]
-    #         print('loop',n,' of ', p_groups,'  group:',srng,':',frng)
-    #         srng+= cpus
-    #         frng+= cpus
-    #         Update_Progress(float(n)/(float(p_groups)-1.))
+    processes= [Process(target=helix_funcs.process_file, args=(f, s)) for f in fs]
+    for process_chunk in chunks(processes, cpu_count()):
+        print('loop ', process_chunk, ' of ', cpu_count())
+        [p.start() for p in process_chunk]
+        [p.join() for p in process_chunk]
     fin_time = time.time()
     print("\nFinished processing in {0:6.2f} min.".format((fin_time-start)/60.))

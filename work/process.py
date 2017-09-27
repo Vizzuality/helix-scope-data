0cb392f02a23f4462200349c2fc0dad15494f491
Benjamin A. Laken <benlaken@gmail.com>
2017-09-27 19:23:39 +0200

    setup to process admin0

--- work/process.py
+++ work/process.py
@@ -10,19 +10,23 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-#s = gpd.read_file('./data/gadm28_adm1/gadm28_adm1.shp')
+# ------------- FOR NOW SET HERE THE SHAPEFILE TO PROCESS AND CHANGE argument--
+# e.g. processes= [Process(target=helix_funcs.process_file, args=(f, s, 0),
+#                                                   0 above for admin-0 ^
+
 # SIMPLIFIED SHAPES FOR ADMIN 0 LEVEL
-#s = gpd.read_file("./data/gadm28_adm1_simplified/gadm28_adm0_simplified.shp")
+s = gpd.read_file("./data/gadm28_adm1_simplified/gadm28_adm0_simplified.shp")
 # SIMPLIFIED SHAPES FOR ADMIN 1 LEVEL
-s = gpd.read_file("./data/gadm28_adm1_simplified/gadm28_adm1_simplified.shp")
+#s = gpd.read_file("./data/gadm28_adm1_simplified/gadm28_adm1_simplified.shp")
+
 s = s.to_crs(epsg='4326')
 d = helix_funcs.identify_netcdf_and_csv_files()
 fs = d.get('nc')  # Simply break this list by the number of avilable processors
 
 if __name__ == "__main__":
     start = time.time()
-    processes= [Process(target=helix_funcs.process_file, args=(f, s),
-                        kwargs={'verbose': True, admin_level=1}) for f in fs]
+    processes= [Process(target=helix_funcs.process_file, args=(f, s, 0),
+                        kwargs={'verbose': True,}) for f in fs]
     for process_chunk in chunks(processes, cpu_count()):
         print('loop ', process_chunk, ' of ', cpu_count())
         [p.start() for p in process_chunk]
